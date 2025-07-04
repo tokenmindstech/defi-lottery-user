4f2b5f8e0a24ee8c37e5e19c486726097024b1d0
Danzrrr <graita.sukma@gmail.com>
2025-07-04 11:09:57 +0700

    feat: base available bonus

--- app/(dashboard)/bonus-reward/_components/item.tsx
+++ app/(dashboard)/bonus-reward/_components/item.tsx
@@ -5,7 +5,6 @@ import {
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
@@ -15,17 +14,15 @@ const ItemPerks = () => {
   return (
     <Card className="bg-bgtext-900 border border-bgtext-800 rounded-xl px-4 py-4">
       <CardHeader className="px-0 py-0">
-        <CardTitle className="w-full text-bgtext-100 font-inter text-xl font-semibold px-0">
-          <div className="flex w-full h-[300px] relative rounded-xl">
-            <Image
-              src="/assets/images/rewards.jpg"
-              alt="Perks"
-              fill
-              className="object-cover rounded-xl"
-              sizes="100%"
-            />
-          </div>
-        </CardTitle>
+        <div className="flex w-full h-[200px] relative rounded-xl bg-linblue-start">
+          <Image
+            src="/assets/images/iphone.png"
+            alt="Perks"
+            fill
+            className="object-contain rounded-xl"
+            sizes="100%"
+          />
+        </div>
         <CardDescription className="hidden"></CardDescription>
       </CardHeader>
       <CardContent className="px-0">
@@ -40,7 +37,7 @@ const ItemPerks = () => {
         <Link href="/lucky-draw/1" className="w-full">
           <Button className="w-full bg-bgtext-800  border border-bgtext-700 hover:bg-bgtext-700 rounded-xl cursor-pointer">
             <p className="text-bgtext-100 font-inter font-medium text-sm py-4">
-              Claim Now
+              More Details
             </p>
           </Button>
         </Link>
--- app/(dashboard)/bonus-reward/page.tsx
+++ app/(dashboard)/bonus-reward/page.tsx
@@ -5,7 +5,7 @@ const PerksPage = () => {
   return (
     <section className="flex flex-col w-full h-full space-y-10">
       <h2 className="text-3xl font-medium text-bgtext-100 font-inter whitespace-nowrap">
-        Available Perks
+        Available Bonus Reward
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-10">
