6ef2991c957dd4c5502795a9be9c21bd06d06172
Danzrrr <graita.sukma@gmail.com>
2025-07-04 10:06:37 +0700

    fix: change marquee to rotating text

--- app/(dashboard)/lucky-draw/_components/winner-banner.tsx
+++ app/(dashboard)/lucky-draw/_components/winner-banner.tsx
@@ -1,7 +1,7 @@
 import { censorString } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
-import Marquee from "react-fast-marquee";
+import RotatingText from "@/components/ui/rotating-text";
 
 interface WinnerBannerProps {
   perks: Perks;
@@ -9,50 +9,41 @@ interface WinnerBannerProps {
 
 const WinnerBanner = ({ perks }: WinnerBannerProps) => {
   return (
-    <div className="relative w-full h-full bg-gradient-to-b from-linprimary-start to-linprimary-end/50 text-bgtext-100 hover:bg-gradient-to-b border-2 border-bgtext-800 rounded-2xl ease-out transition-all duration-300">
-      <div className="relative overflow-visible">
-        <Marquee
-          direction="left"
-          speed={30}
-          gradient={false}
-          className="flex flex-row items-center space-x-5 p-4 overflow-x-hidden "
-        >
-          <p className="text-sm font-inter font-light whitespace-nowrap pr-3">
-            Congratulations to{" "}
-            {perks.perkWinners.map((winner, index) => (
-              <span key={winner.id} className="font-medium">
-                {censorString(winner.name)}
-                {index < perks.perkWinners.length - 1
-                  ? index === perks.perkWinners.length - 2
-                    ? " and "
-                    : ", "
-                  : ""}
-              </span>
-            ))}
-          </p>
+    <div className="relative flex flex-row items-center justify-between space-x-5 w-full h-full p-4 bg-gradient-to-b from-linprimary-start to-linprimary-end/50 text-bgtext-100 hover:bg-gradient-to-b border-2 border-bgtext-800 rounded-2xl ease-out transition-all duration-300 overflow-visible">
+      <div className="flex flex-row space-x-2 items-center relative">
+        <p className="text-sm font-inter font-light whitespace-nowrap">
+          Congratulations to{" "}
+        </p>
+        <RotatingText
+          texts={perks.perkWinners.map((winner) => censorString(winner.name))}
+          mainClassName="text-bgtext-100 font-bold"
+          staggerFrom={"last"}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-120%" }}
+          staggerDuration={0.025}
+          splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+          transition={{ type: "spring", damping: 30, stiffness: 400 }}
+          rotationInterval={4000}
+        />
+      </div>
 
-          <div className="flex flex-row items-center relative">
-            <p className="text-sm font-inter font-medium whitespace-nowrap mr-2">
-              You&apos;ve won this week&apos;s bonus reward:{" "}
-            </p>
+      <div className="flex flex-row items-center relative">
+        <p className="hidden lg:block text-sm font-inter font-medium whitespace-nowrap">
+          You&apos;ve won this week&apos;s bonus reward:{" "}
+        </p>
 
-            <div className="relative w-[70px]">
-              <Image
-                src={perks.imageUrl}
-                alt={perks.name}
-                width={80}
-                height={80}
-                className="absolute object-cover rounded-lg shadow-2xl transform -translate-y-8 scale-110 rotate-3 hover:scale-125 hover:-translate-y-10 transition-all duration-300 z-50"
-                draggable={false}
-                loading="lazy"
-                style={{
-                  filter: "drop-shadow(0 10px 20px rgba(0,0,0,0.3))",
-                  transformOrigin: "center bottom",
-                }}
-              />
-            </div>
-          </div>
-        </Marquee>
+        <div className="relative w-[60px]">
+          <Image
+            src={perks.imageUrl}
+            alt={perks.name}
+            width={60}
+            height={60}
+            className="object-cover rounded-lg absolute -top-13 right-0 z-10 shadow-lg"
+            draggable={false}
+            loading="lazy"
+          />
+        </div>
       </div>
     </div>
   );
