f18384cbf0ca51d972c5e3878b48db45959bcbb4
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2025-07-30 16:51:55 +0700

    Update claim-reward-button.tsx

--- app/(dashboard)/claim-rewards/_components/claim-reward-button.tsx
+++ app/(dashboard)/claim-rewards/_components/claim-reward-button.tsx
@@ -7,7 +7,6 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
